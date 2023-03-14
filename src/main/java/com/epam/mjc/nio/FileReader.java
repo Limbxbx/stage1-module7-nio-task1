d59d7510e444fab3de2e45d9d62abe8b63c6ad19
Илья <ilyich223@gmail.com>
2023-03-14 11:59:59 +0200

    Attempt #1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -3,9 +3,31 @@ package com.epam.mjc.nio;
 import java.io.File;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) throws IOException {
+        String data = new String(Files.readAllBytes(file.toPath()));
+        Map<String, String> keyValues = parseData(data);
+        String name = keyValues.get("Name");
+        Integer age = Integer.parseInt(keyValues.get("Age"));
+        String email = keyValues.get("Email");
+        Long phone = Long.parseLong(keyValues.get("Phone"));
+        return new Profile(name, age, email, phone);
+    }
+
+    private static Map<String, String> parseData(String data) {
+        Map<String, String> keyValues = new HashMap<>();
+        String[] lines = data.split("\n");
+        for (String line : lines) {
+            String[] keyValue = line.split(": ");
+            keyValues.put(keyValue[0], keyValue[1].trim());
+        }
+        return keyValues;
     }
 }
